48f011c71cae9bb0a69373274e047c9e989621e0
Zhero-Six <vmochoge@gmail.com>
2025-05-13 22:56:38 +0300

    updates

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,29 @@
-#!/usr/bin/env python3
+# lib/book.py
 
 class Book:
-    pass
-    
-        
+    def __init__(self, title, page_count):
+        """Initialize a Book with title and page_count"""
+        self._title = title
+        self._page_count = page_count
+
+    @property
+    def title(self):
+        """Get the book title"""
+        return self._title
+
+    @property
+    def page_count(self):
+        """Get the page count"""
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, value):
+        """Set the page count with type validation"""
+        if not isinstance(value, int):
+            print("page_count must be an integer")
+        else:
+            self._page_count = value
+
+    def turn_page(self):
+        """Simulate turning a page"""
+        print("Flipping the page...wow, you read fast!")
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,31 @@
-#!/usr/bin/env python3
+# lib/shoe.py
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        """Initialize a Shoe with brand and size"""
+        self._brand = brand
+        self._size = size
+        self.condition = None  # Initialize condition attribute
+
+    @property
+    def brand(self):
+        """Get the shoe brand"""
+        return self._brand
+
+    @property
+    def size(self):
+        """Get the shoe size"""
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        """Set the shoe size with type validation"""
+        if not isinstance(value, int):
+            print("size must be an integer")
+        else:
+            self._size = value
+
+    def cobble(self):
+        """Repair the shoe and set condition to New"""
+        print("Your shoe is as good as new!")
+        self.condition = "New"
